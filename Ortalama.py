0171be4004d77e202743a8d0f96cd83d182b32c4
mfeyzi <45923793+mfeyzi@users.noreply.github.com>
2018-12-17 00:39:18 +0300

    Add files via upload

---
+++ Ortalama.py
@@ -0,0 +1,16 @@
+print("Öğrenci Ortalama Hesaplama")
+
+A = float(input("1.Öğrenci ortalama giriniz:"))
+B = float(input("2.Öğrenci ortalama giriniz:"))
+C = float(input("3.Öğrenci ortalama giriniz:"))
+D = float(input("4.Öğrenci ortalama giriniz:"))
+E = float(input("5.Öğrenci ortalama giriniz:"))
+F = float(input("6.Öğrenci ortalama giriniz:"))
+G = float(input("7.Öğrenci ortalama giriniz:"))
+H = float(input("8.Öğrenci ortalama giriniz:"))
+I = float(input("9.Öğrenci ortalama giriniz:"))
+J = float(input("10.Öğrenci ortalama giriniz:"))
+
+ORTALAMA = (A + B + C + D + E + F + G + H + I + J) / 10
+
+print("Ortalamaniz:", ORTALAMA)
